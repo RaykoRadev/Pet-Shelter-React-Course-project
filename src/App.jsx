921a20d85db6d2da9bebf03440258baa15d19369
RaykoRadev <christmas898@gmail.com>
2025-11-06 14:43:46 +0100

    adding base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+
 function App() {
-    return;
+    return (
+        <div className="min-h-screen bg-green-200 text-black overflow-hidden">
+            <Navbar />
+            <Login />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
