b5c1a3dbd2146371135b5ecaff8c7d57895bbddf
RaykoRadev <christmas898@gmail.com>
2025-11-20 11:58:48 +0100

    removing unused code

--- src/components/catalog-item/CatalogItem.jsx
+++ src/components/catalog-item/CatalogItem.jsx
@@ -33,29 +33,11 @@ export default function CatalogItem({
                         <h3 className="text-sm lg:text-[15px] font-semibold text-slate-900  mt-2">
                             Breed: {breed}
                         </h3>
-                        <h4 className="text-sm lg:text-base text-blue-600 font-bold mt-2">
+                        <h4 className="text-sm lg:text-[15px] font-semibold text-slate-900  mt-2">
                             Age: {age}
                         </h4>
                     </div>
                     {/*// todo  if the logic about loged not owner is easy to be transfered in this component  */}
-                    {/* <div className="flex justify-center space-x-1 mt-4">
-                        <div
-                            className="border-pink-200  flex items-center justify-center rounded-sm cursor-pointer"
-                            title="Wishlist"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="25px"
-                                className="fill-pink-600 inline-block transition-transform duration-300 transform hover:scale-125"
-                                viewBox="0 0 64 64"
-                            >
-                                <path
-                                    d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                                    data-original="#000000"
-                                />
-                            </svg>
-                        </div>
-                    </div> */}
                 </div>
             </Link>
         </div>
