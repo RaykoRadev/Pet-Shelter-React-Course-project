a06ea51fe7eb1b73af2e381b9a76b23f20a2aa8e
RaykoRadev <christmas898@gmail.com>
2025-11-30 13:22:05 +0100

    adding env

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,9 @@
 export const UPLOAD_PHOTO_URL = "https://api.imgbb.com/1/upload?key=";
 export const API_imgBB_KEY = "c74f2163636580193aeb4d942fc5ccbd";
 
-const host = "http://localhost:3000";
+const host = import.meta.env.API_URL || "http://localhost:3000";
+// const host = "http://localhost:3000";
+const online = "https://back-end-server-nodejs-express.onrender.com";
 
 export const endpoints = {
     login: host + "/users/login",
